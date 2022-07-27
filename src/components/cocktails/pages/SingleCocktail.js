2f92f41ffebfb46666d5e1c7638bf0ae1c3aad69
ulugbekmahammadjonov <ulugbekmahammadjonov1218@gmail.com>
2022-07-27 21:39:18 +0500

    project6, lesson24

--- src/components/cocktails/pages/SingleCocktail.js
+++ src/components/cocktails/pages/SingleCocktail.js
@@ -1,33 +1,108 @@
 import React from "react";
-import Cocktail from "./Cocktail";
-import Loading from "./Loading";
+import Loading from "../Loading";
+import { useParams, Link } from "react-router-dom";
 
-import { useGlobalContext } from "../../context";
-const CockTailList = () => {
-  const { cocktails, loading } = useGlobalContext();
-  console.log(cocktails);
+const SingleCocktail = () => {
+  const { id } = useParams();
+  const [loading, setLoading] = React.useState(false);
+  const [cocktail, setCocktail] = React.useState(null);
+
+  React.useEffect(() => {
+    setLoading(true);
+    async function getCocktail() {
+      try {
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+        );
+        const data = await response.json();
+        console.log(data);
+        if (data.drinks) {
+          const {
+            strDrink: name,
+            strDrinkThumb: image,
+            strAlcoholic: info,
+            strCategory: category,
+            strGlass: glass,
+            strInstructions: instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          } = data.drinks[0];
+          const ingredients = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ];
+          const newCocktail = {
+            name,
+            image,
+            info,
+            category,
+            glass,
+            instructions,
+            ingredients,
+          };
+          setCocktail(newCocktail);
+        } else {
+          setCocktail(null);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+    getCocktail();
+  }, [id]);
 
   if (loading) {
     return <Loading />;
   }
 
-  if (cocktails.length < 1) {
+  if (!cocktail) {
+    return <h2 className="section-title">koktaillar yo'q</h2>;
+  } else {
+    const { name, image, category, info, glass, instructions, ingredients } =
+      cocktail;
     return (
-      <h2 className="section-title">
-        no cocktails matched your search criteria
-      </h2>
+      <section className="section cocktail-section">
+        <Link to="/" className="btn btn-primary">
+          back home
+        </Link>
+        <h2 className="section-title">{name}</h2>
+        <div className="drink">
+          <img src={image} alt={name} />
+
+          <div className="drink-info">
+            <p>
+              <span className="drink-data">name:</span> {name}
+            </p>
+            <p>
+              <span className="drink-data">category:</span> {category}
+            </p>
+            <p>
+              <span className="drink-data">info:</span> {info}
+            </p>
+            <p>
+              <span className="drink-data">glass:</span> {glass}
+            </p>
+            <p>
+              <span className="drink-data">instructions:</span> {instructions}
+            </p>
+            <p>
+              <span className="drink-data">ingredients:</span>
+              {ingredients.map((item, index) => {
+                return item ? <span key={item}>{item}</span> : null;
+              })}
+            </p>
+          </div>
+        </div>
+      </section>
     );
   }
-  return (
-    <section className="section">
-      <h2 className="section-title">cocktails</h2>
-      <div className="cocktails-center">
-        {cocktails.map((item) => {
-          return <Cocktail key={item.id} {...item} />;
-        })}
-      </div>
-    </section>
-  );
 };
 
-export default CockTailList;
+export default SingleCocktail;
